305b6232344d87edbef1c38e2c2af8eaf3f3d5cc
Vitaliy Derda <derdavitaliy@gmail.com>
2024-05-26 12:40:06 +0300

    feat: added car rental solution

---
+++ Customer.cs
@@ -0,0 +1,48 @@
+﻿namespace CarRental;
+
+public class Customer
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string IdNumber { get; set; }
+    public string PassportNumber { get; set; }
+    public string DrivingLicenseNumber { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public DateTime? RentDate { get; set; }
+    public DateTime? PurchaseDate { get; set; }
+    public string Gender { get; set; }
+
+    public event EventHandler<Car> CarRented;
+    public event EventHandler<Car> CarBought;
+    public event EventHandler<decimal> MoneyPaid;
+
+    public void RentCar(Car car)
+    {
+        RentDate = DateTime.Now;
+
+        Console.WriteLine($"Car rented on {RentDate.Value}");
+        
+        CarRented?.Invoke(this, car);
+    }
+
+    public void BuyCar(Car car)
+    {
+        PurchaseDate = DateTime.Now;
+
+        Console.WriteLine($"Car bought on {PurchaseDate.Value}");
+        
+        CarBought?.Invoke(this, car);
+    }
+
+    public void ReturnCar(Car car)
+    {
+        Console.WriteLine("Car returned.");
+    }
+
+    public void PayMoney(decimal amount)
+    {
+        Console.WriteLine($"Paid {amount}");
+
+        MoneyPaid?.Invoke(this, amount);
+    }
+}
---
+++ Inspection.cs
@@ -0,0 +1,10 @@
+﻿namespace CarRental;
+
+public class Inspection
+{
+    public string InspectionId { get; set; }
+    public DateTime Date { get; set; }
+    public Inspector Inspector { get; set; }
+    public InspectionResult Result { get; set; }
+    public string CarId { get; set; }
+}
